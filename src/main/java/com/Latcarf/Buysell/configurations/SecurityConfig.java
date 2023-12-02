b6884742ae81c7795064d5934fc84847f138badd
Latcarf <topmail2222@gmail.com>
2023-12-02 14:48:08 +0200

    Code readability improvements Changes to the parameter creating a new user

--- src/main/java/com/Latcarf/Buysell/configurations/SecurityConfig.java
+++ src/main/java/com/Latcarf/Buysell/configurations/SecurityConfig.java
@@ -21,16 +21,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/product/**", "/images/**", "/registration", "/user/**")
-                .permitAll()
-                .anyRequest().authenticated()
+                    .antMatchers("/", "/product/**", "/images/**", "/registration", "/user/**")
+                    .permitAll()
+                    .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
+                    .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
                 .and()
-                .logout()
-                .permitAll();
+                    .logout()
+                    .permitAll();
     }
 
     @Override
